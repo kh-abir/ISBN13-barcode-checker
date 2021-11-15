57460fb930b1bc3e6dbdfdbd357b045f72503c4d
kamrul Hassan <kh27.abir@gmail.com>
2021-11-15 19:59:08 +0600

    include algorithm for reference as comment

--- isbn13_barcode_checker.rb
+++ isbn13_barcode_checker.rb
@@ -1,3 +1,8 @@
+# 1. Take each digit, from left to right and multiply them alternatively by 1 and 3
+# 2. Sum those numbers
+# 3. Take mod 10 of the summed figure
+# 4. Subtract 10 and if the end number is 10, make it 0
+
 digits = gets.chomp
 sum = 0
 digits.each_char.with_index { |digit,index| sum += index % 2 == 0 ? digit.to_i * 1 : digit.to_i * 3 }
